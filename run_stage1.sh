724ae3d3f7a33a96346a273f594be4cfe930a7a5
Wei Song (SII) <80997213+songweii@users.noreply.github.com>
2026-04-21 01:43:43 +0800

    Update torchrun parameters and data path

--- run_stage1.sh
+++ run_stage1.sh
@@ -1,11 +1,11 @@
 export OMP_NUM_THREADS=8
-torchrun --nproc_per_node 2 -m main \
+torchrun --nproc_per_node 8 -m main \
     --sem_weight 1 \
     --stage 1 \
     --name siglip2-384-rvq8-32d-stage1 \
     --model "model_config_siglip_384_rvq8_32d" \
     --save-frequency 1 \
-    --train-data="/mnt/public/users/songwei/data_zoo/data_tokenizer/cc12/cc12m-train-{0000..2175}.tar" \
+    --train-data="$YOUR_DATA_PATH/cc12/cc12m-train-{0000..2175}.tar" \
     --train-num-samples 10000000 \
     --dataset-type "webdataset" \
     --warmup=10000 \
